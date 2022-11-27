8277f1811162021f328adea1083a3df330c2c934
AlbertoSanchez19 <albers19@ucm.es>
2022-11-27 18:12:35 +0100

    Codegen changes

--- Real-Time Face Recognition/face_detection.m
+++ Real-Time Face Recognition/face_detection.m
@@ -1,21 +1,25 @@
+function face_detection()
+%#codegen
 rpi = raspi("192.168.1.20", "pi", "raspberry");
 cam = cameraboard(rpi, "Resolution", "640x480");
+%cam = webcam();
 
 % Cargamos la red neuronal que hemos creado. "net.mat" se ha creado con el
 % comando save("net.mat", "net");
 net = coder.loadDeepLearningNetwork("./SAVED_OBJECTS/net.mat");
 
 % ---  ESTO ES PARA COD, LO HE CAMBIADO AHORA POR YOLOv4 ---
-% load("detector.mat");
+%load("SAVED_OBJECTS\detector.mat");
+detector = coder.loadDeepLearningNetwork("SAVED_OBJECTS\detector.mat");
 % Usamos un Vision Cascade Detector para detectar las caras. Una vez nos
 % detecte donde está la cara, usaremos esa información junto a la red que
 % hemos creado para saber qué persona es.
-face_detector = vision.CascadeObjectDetector;
+%face_detector = vision.CascadeObjectDetector;
 
 % Este parámetro, si lo aumentamos, nos puede ayudar a detectar de una
 % manera más precisa las diferentes caras que se encuentren en la imagen
 % (en nuestro caso solo una)
-face_detector.MergeThreshold = 4;
+%face_detector.MergeThreshold = 4;
 
 input_size = [224,224,3];
 
@@ -25,8 +29,7 @@ text2display = "......";
 % actual, y toc lo utiliza para ver el tiempo transcurrido.
 start = tic;
 fprintf("Entering into while loop.\n");
-
-bounding_boxes = [];
+bounding_boxes = zeros(1,4,'single');
 while true
     % Saca una captura de la cámara
     img = snapshot(cam);
@@ -34,18 +37,21 @@ while true
     elapsed_time = toc(start);
 
     % Procesa un frame cada segundo
-    if(elapsed_time > 1)
+    if(elapsed_time > 0.01)
 
         % YOLOv4 -> Obtenemos las "cajas" donde se encuentras las caras.
-        %[bounding_boxes,scores,labels] = detect(detector,img);
+        [bounding_boxes,scores,labels] = detect(detector,img);
 
         % COD
-        bounding_boxes = face_detector(img);
+        %bounding_boxes = face_detector(img);
         
         % Si no es vacío, es decir, ha detectado alguna cara.
         if ~isempty(bounding_boxes)
             % La recortamos para solo quedarnos con la cara detectado.
             img_processed = imcrop(img, bounding_boxes(1,:));
+            % Para que cuando lo implemento en la Rasp, sepa que la matriz
+            % tiene "dimensión 3" en la tercera dimensión.
+            img_processed = img_processed(:,:, 1:3);
 
             % Adaptamos el tamaño de la imagen a la entrada de la red.
             img_resized = imresize(img_processed, input_size(1:2));
@@ -59,14 +65,14 @@ while true
         end
     end
 
+    % Para la generación de código, text2display necesita ser de tipo cell
+    % en vez de string
+    ctext2display = cellstr(text2display);
     % Insertamos en img, en la posición 0,0, el texto previamente calculado
-    img_label = insertText(img, [0,0], text2display);
+    img_label = insertText(img, [0,0], ctext2display);
     % Actualizamos el cuadrado solamente si ha detectado una cara.
     if ~isempty(bounding_boxes)
         img_label = insertObjectAnnotation(img_label, "rectangle", bounding_boxes, "Face", "LineWidth", 2);
     end
     imshow(img_label);
 end
-
-clear rpi;
-clear cam;
